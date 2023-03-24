740e1c8924b5555e805a88174001fe1be0cce638
wangxuewen02 <wangxuewen02@MacintoshdeMacBook-Pro.local>
2023-03-24 18:26:38 +0800

    v1.2.2.2

--- models/service/page/base/Page.php
+++ models/service/page/base/Page.php
@@ -4,7 +4,27 @@ class Service_Page_Base_Page extends Zy_Core_Service{
 
     // 查询通过登录uid上课俩表, 
     public function execute () {
+        if (!$this->checkTeacher()) {
+            return ;  
+        }
 
+        $sts = strtotime(date("Y-m-1"));
+        $ets = strtotime(date('Y-m-d', strtotime('first day of next month')));
 
+        $total = 0;
+        $conds = array(
+            "start_time >= " . $sts,
+            "end_time < " . $ets,
+            "teacher_id" => $this->adption["userid"],
+        );
+        $serviceSchedule = new Service_Data_Schedule();
+        $lists = $serviceSchedule->getListByConds($conds, array('start_time', "end_time"));
+        if (!empty($lists)) {
+            foreach ($lists as $item) {
+                $timeLength = ($item['end_time'] - $item['start_time']) / 3600;
+                $total += $timeLength;
+            }
+        }
+        return array("total" => $total);
     }
 }
